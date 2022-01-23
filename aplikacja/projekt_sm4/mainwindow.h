98aa92d5fd160b9e19a62cf6841ef35e924c187e
asiulec <97520500+asiulec@users.noreply.github.com>
2022-01-23 22:31:04 +0100

    Add files via upload

---
+++ aplikacja/projekt_sm4/mainwindow.h
@@ -0,0 +1,56 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtSerialPort>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+protected:
+    void WriteData(const QString &msg);
+
+private slots:
+    void on_pushButton_szukaj_clicked();
+
+    void on_pushButton_polacz_clicked();
+
+    void on_pushButton_rozlacz_clicked();
+
+    void on_pushButton_wyczysc_clicked();
+
+    void ReadData();
+
+    void on_pushButton_stop_clicked();
+
+    void on_pushButton_wyslij_v2_clicked();
+
+    void on_pushButton_odbierz_v2_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    void addToLogs(QString message);
+    QSerialPort *device;
+
+    int wartosc;
+    QString arg1;
+    QByteArray buffer;
+    QList<QString> data;
+    const char* dane;
+    char a;
+
+
+};
+#endif // MAINWINDOW_H
